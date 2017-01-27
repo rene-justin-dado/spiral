88bb876483d95390e61e37c7153d506ce21d78b0
Justin Dado <justindado@yahoo.com>
2017-01-27 22:04:42 +1300

    testing for complete Board constructor

--- SpiralTests/ProgramTests.cs
+++ SpiralTests/ProgramTests.cs
@@ -24,5 +24,19 @@ namespace SpiralTests
             Assert.Equal(5, board.Cells.Length);
             Assert.Equal(5, board.Cells[0].Length);
         }
+
+        [Fact]
+        public void BoardGeneratedSixByTwo()
+        {
+            // Arrange
+            string expected = " #\n# \n #\n# \n #\n# \n";
+
+            // Act
+            var board = new Board(6, 2);
+            var actual = board.ToString();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
     }
 }
