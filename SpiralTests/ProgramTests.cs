8651b76c1b4e4b42e42da6e08d27179e150e815c
Justin Dado <justindado@yahoo.com>
2017-01-27 19:30:06 +1300

    updated tests

--- SpiralTests/ProgramTests.cs
+++ SpiralTests/ProgramTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Spiral;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,15 @@ namespace SpiralTests
         public void Working()
         {
             Assert.True(true);
+        }
+
+        [Fact]
+        public void BoardGeneratedWithCorrectDimensions()
+        {
+            var board = new Board(5, 5);
+
+            Assert.Equal(5, board.Cells.Length);
+            Assert.Equal(5, board.Cells[0].Length);
         }
     }
 }
